53a8a38b99c04717cf8ebf02efee708c98b55d29
シロキ猫 <inoli1227@gmail.com>
2026-05-09 00:20:48 +0900

    e

--- script.js
+++ script.js
@@ -23,12 +23,10 @@ function navigate(page) {
 // 初期表示
 navigate("home");
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./sw.js")
-    .then(() => {
-      console.log("Service Worker 登録成功");
-    })
-    .catch((err) => {
-      console.error("登録失敗", err);
-    });
-}
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js")
+      .then(() => console.log("SW登録成功"))
+      .catch(err => console.error("SW登録失敗", err));
+  }
+});
